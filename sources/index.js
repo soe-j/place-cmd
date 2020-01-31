b813dc0206cb82b8760638f6829d78d426366dba
soe-j <soeda@val.co.jp>
2020-01-31 17:41:23 +0900

    wip

--- sources/index.js
+++ sources/index.js
@@ -1,3 +1,16 @@
-function doGet(event) {
+function doPost(event) {
+
+  var inputText = e.parameter.text;
+  var channelName = e.parameter.channel_id;
+
+  var Place = new Place(inputText);
+
   return ContentService.createTextOutput(JSON.stringify(event)).setMimeType(ContentService.MimeType.JSON);
+}
+
+// for Local Test
+if (typeof module !== 'undefined') {
+  module.exports = {
+    doPost
+  }
 }
